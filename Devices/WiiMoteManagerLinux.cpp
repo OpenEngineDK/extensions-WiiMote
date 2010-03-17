67276d93de3b4f5b7c461180a2da7e242709dff4
ptx <ptx@openengine.dk>
2010-03-17 16:54:45 +0000

    Added stub linux implementation

---
+++ Devices/WiiMoteManagerLinux.cpp
@@ -0,0 +1,19 @@
+#include <Devices/WiiMoteManager.h>
+#include <Devices/WiiMote.h>
+#include <Logging/Logger.h>
+
+
+namespace OpenEngine {
+namespace Devices {
+
+
+WiiMoteManager::WiiMoteManager() {
+    logger.info << "implement me wii thingy" << logger.end;    
+}
+
+void WiiMoteManager::LookForMote() {
+
+}
+
+}
+}
